b33ec8af2210d494909615e620b4bc15fdfb4af5
rutmir <rutmir@mail.ru>
2017-04-09 20:02:40 +0300

    updated logger functionality

--- core/log/log.go
+++ core/log/log.go
@@ -6,6 +6,8 @@ import (
 	"strings"
 	"strconv"
 	"runtime"
+	"net/http"
+	"time"
 )
 
 type Level byte
@@ -27,6 +29,16 @@ const (
 )
 
 var settings *LogSettings
+
+// HttpLogger - logger for every http requests
+func HttpLogger(inner http.Handler, name string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		inner.ServeHTTP(w, r)
+		Info("%s\t%s\t%s\t%s\n", r.Method, r.RequestURI, name, time.Since(start))
+	})
+}
+
 
 /*API*/
 
